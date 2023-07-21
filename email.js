82e47d4d39d759dee77368a00f9ce6195d1039d2
Megh Patel <91938737+MeGH2711@users.noreply.github.com>
2023-07-21 20:27:18 +0530

    Change in Landing page for laptop

--- email.js
+++ email.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 function SendMail() {
     var params = {
         name: document.getElementById("name").value,
@@ -23,4 +24,31 @@ function SendMail() {
             modal.show();
         })
         .catch(err => console.log(err));
+=======
+function SendMail() {
+    var params = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        phone: document.getElementById("phone").value,
+        subject: document.getElementById("subject").value,
+        message: document.getElementById("message").value,
+    };
+
+    const serviceID = "service_04zymjf";
+    const templateID = "template_c2esn7l";
+
+    emailjs.send(serviceID, templateID, params)
+        .then(res => {
+            document.getElementById("name").value = "";
+            document.getElementById("email").value = "";
+            document.getElementById("phone").value = "";
+            document.getElementById("subject").value = "";
+            document.getElementById("message").value = "";
+            console.log(res);
+            // Show the modal
+            var modal = new bootstrap.Modal(document.getElementById("exampleModal"));
+            modal.show();
+        })
+        .catch(err => console.log(err));
+>>>>>>> origin/main
 }
