97148c18c0dbf59a8ded9ec450a27267d33a3c8b
Megh Patel <91938737+MeGH2711@users.noreply.github.com>
2023-08-09 21:14:23 +0530

    reframed email js file

--- email.js
+++ email.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function SendMail() {
     var params = {
         name: document.getElementById("name").value,
@@ -19,36 +18,6 @@ function SendMail() {
             document.getElementById("subject").value = "";
             document.getElementById("message").value = "";
             console.log(res);
-            // Show the modal
-            var modal = new bootstrap.Modal(document.getElementById("exampleModal"));
-            modal.show();
         })
         .catch(err => console.log(err));
-=======
-function SendMail() {
-    var params = {
-        name: document.getElementById("name").value,
-        email: document.getElementById("email").value,
-        phone: document.getElementById("phone").value,
-        subject: document.getElementById("subject").value,
-        message: document.getElementById("message").value,
-    };
-
-    const serviceID = "service_04zymjf";
-    const templateID = "template_c2esn7l";
-
-    emailjs.send(serviceID, templateID, params)
-        .then(res => {
-            document.getElementById("name").value = "";
-            document.getElementById("email").value = "";
-            document.getElementById("phone").value = "";
-            document.getElementById("subject").value = "";
-            document.getElementById("message").value = "";
-            console.log(res);
-            // Show the modal
-            var modal = new bootstrap.Modal(document.getElementById("exampleModal"));
-            modal.show();
-        })
-        .catch(err => console.log(err));
->>>>>>> origin/main
 }
