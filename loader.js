71a9125a13b91369a1d4e9f8b2342fdbb6ae3ed0
Megh Patel <91938737+MeGH2711@users.noreply.github.com>
2023-09-09 12:42:35 +0530

    Add files via upload

--- loader.js
+++ loader.js
@@ -1,12 +1,18 @@
-
-// After 10 seconds, hide the loader and display the content
+// After 10 seconds, fade out the loader, fade in the content, and enable scrolling
 setTimeout(function () {
     const loader = document.getElementById('loader-on');
     const content = document.getElementById('loader-off');
 
-    // Hide the loader by setting its display to 'none'
-    loader.style.display = 'none';
+    // Fade out the loader
+    loader.style.opacity = '0';
 
-    // Show the content by setting its display to 'block'
-    content.style.display = 'block';
-}, 5000); // 10 seconds (10000 milliseconds)
+    // Wait for the loader to fade out and then hide it
+    setTimeout(function () {
+        loader.style.display = 'none';
+
+
+        // Fade in the content
+        content.style.opacity = '1';
+        content.style.display = 'block';
+    }, 500); // Wait for 0.5 seconds (500 milliseconds)
+}, 3000); // 10 seconds (10000 milliseconds)
